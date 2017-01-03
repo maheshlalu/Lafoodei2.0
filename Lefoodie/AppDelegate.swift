d974f9ff8cd7b8a1c3251b4e9f211f142ed754c6
CX One <Manishi@CX-Ones-Mac-mini.local>
2017-01-03 12:02:44 +0530

    boo

--- Lefoodie/AppDelegate.swift
+++ Lefoodie/AppDelegate.swift
@@ -29,6 +29,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //self.setUpMagicalDB()
         print( getDocumentsDirectory())
 
+        
+        
         return true
     }
     
